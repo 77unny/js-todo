d249e2880535b00195cb669d5ec0256506c3b028
baekCode <sosaer88@gmail.com>
2020-04-17 01:48:57 +0900

    feat: layout render

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -1,5 +1,9 @@
 const tag = '[app]';
 import { getEl } from './utils/common';
-import { drawHeader } from './component/Header';
+import { drawHeader } from './component/layout/header';
+import { drawBody } from './component/layout/body';
+import { drawFooter } from './component/layout/footer';
 const root = getEl('#wrap');
-root.insertAdjacentHTML('afterbegin', drawHeader);
+root.insertAdjacentHTML('beforeend', drawHeader);
+root.insertAdjacentHTML('beforeend', drawBody);
+root.insertAdjacentHTML('beforeend', drawFooter);
