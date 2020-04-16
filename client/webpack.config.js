817cd7e082866b5db69aef757d6e797246fcfe77
baekCode <sosaer88@gmail.com>
2020-04-16 23:27:38 +0900

    add: TODO project setting

---
+++ client/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: ['./src/js/app.js'],
+    output: {
+        path: path.resolve(__dirname, 'src/dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.ts$/,
+                use: ['ts-loader'],
+                exclude: /node_modules/,
+            },
+        ],
+    },
+};
