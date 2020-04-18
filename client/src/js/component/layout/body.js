0d62c182e632d04a76fdc696d3de31f10330b393
baekCode <sosaer88@gmail.com>
2020-04-18 23:58:08 +0900

    chore: 레이아웃 구조 변경

---
+++ client/src/js/component/layout/body.js
@@ -0,0 +1,6 @@
+const drawBody = `
+<div id="container">
+    <div id="todo-app"></div>
+</div>
+<div id="dimmed"></div>`;
+export { drawBody };
---
+++ client/src/js/component/layout/footer.js
@@ -0,0 +1,6 @@
+const drawFooter = `
+<footer>
+    <p>TODOLIST TeamProject</p>
+    <button id="logout">logout</button>
+</footer>`;
+export { drawFooter };
