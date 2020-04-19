72854090949620edfab65a665ab984e29b90cee9
baekCode <sosaer88@gmail.com>
2020-04-19 20:34:15 +0900

    feat: 컬럼에서 전달받은 데이터로 카드를 그림

--- client/src/js/component/todo/card/index.js
+++ client/src/js/component/todo/card/index.js
@@ -1,18 +1,22 @@
-const temp = `
-<div class="card-item content-wrap" draggable="true" data-type="card" tabindex="0">
-    <div class="card-contents">컨텐츠내용</div>
-    <p class="card-writer">added by <span>닉네임</span></p>
-    <button class="btn btn-close">
-        <span class="material-icons">close</span>
-    </button>
-</div>
-`;
+const cardTemplate = ({ id, content }) => {
+    return `
+    <div class="card-item content-wrap" draggable="true" data-type="card" tabindex="0">
+        <div class="card-contents">${content}</div>
+        <p class="card-writer">added by <span>닉네임</span></p>
+        <button class="btn btn-close">
+            <span class="material-icons">close</span>
+        </button>
+    </div>
+    `;
+};
 class TodoCard {
-    constructor() {
+    constructor(target, data) {
+        this.target = target;
+        this.data = data;
         this.render();
     }
     render() {
-        console.log('TodoCard');
+        return this.target.cardWrap.insertAdjacentHTML('beforeend', cardTemplate(this.data));
     }
 }
 export { TodoCard };
